18b975c2d8322814e97bfb8c6cb07f83466f5f20
Raja Swathi <sr16e@my.fsu.edu>
2018-12-14 13:11:26 -0500

    Secure Storage

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import  { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -137,6 +138,23 @@ const FavoritesNavigator = createStackNavigator({
   })
 })
 
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login }
+}, {
+navigationOptions: ({ navigation }) => ({
+  headerStyle: {
+      backgroundColor: "#512DA8"
+  },
+  headerTitleStyle: {
+      color: "#fff"            
+  },
+  headerTintColor: "#fff",
+  headerLeft: <Icon name="menu" size={24}
+    iconStyle={{ color: 'white' }} 
+    onPress={ () => navigation.toggleDrawer() } />    
+})
+});
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
       <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -154,6 +172,21 @@ const CustomDrawerContentComponent = (props) => (
   );
 
 const MainNavigator = createDrawerNavigator({
+  Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
     Home: 
       { screen: HomeNavigator,
         navigationOptions: {
@@ -245,6 +278,7 @@ const MainNavigator = createDrawerNavigator({
           }
         }  
 }, {
+  initialRouteName: 'Home',
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
