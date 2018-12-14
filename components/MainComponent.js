b6d284b698057a15a89b1d41aab89a7c229daff6
Raja Swathi <sr16e@my.fsu.edu>
2018-12-14 15:54:10 -0500

    Picking an image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -139,7 +139,7 @@ const FavoritesNavigator = createStackNavigator({
 })
 
 const LoginNavigator = createStackNavigator({
-  Login: { screen: Login }
+  Login: Login
 }, {
 navigationOptions: ({ navigation }) => ({
   headerStyle: {
@@ -148,6 +148,7 @@ navigationOptions: ({ navigation }) => ({
   headerTitleStyle: {
       color: "#fff"            
   },
+  title: 'Login',
   headerTintColor: "#fff",
   headerLeft: <Icon name="menu" size={24}
     iconStyle={{ color: 'white' }} 
